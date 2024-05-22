2a9d1f41b62a3eba7ef68986e8d69ae64ac8aff3
sosad234 <144006168+sosad234@users.noreply.github.com>
2024-05-22 12:32:29 +0400

    Delete medium directory

--- medium/bank_services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .credit import calculate_credit
-from .installment import calculate_installment
-from .deposit import calculate_deposit
--- medium/bank_services/installment.py
+++
@@ -1,5 +0,0 @@
-def calculate_installment(amount, rate, term):
-    # здесь реализация расчета рассрочки
-    # предположим, что рассрочка без процентов
-    monthly_payment = amount / term
-    return monthly_payment
